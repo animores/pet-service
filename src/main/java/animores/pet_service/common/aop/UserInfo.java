ed5a2030212884be96fb1f2846e5edb68849e504
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-12-01 15:42:05 +0900

    Fix: userinfo 더 깔끔하게 적용할 수 있도록 변경

--- src/main/java/animores/pet_service/common/aop/UserInfo.java
+++ src/main/java/animores/pet_service/common/aop/UserInfo.java
@@ -1,12 +1,17 @@
 package animores.pet_service.common.aop;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@SecurityRequirement(name = "Authorization")
+@PreAuthorize("hasRole('USER')")
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface UserInfo {
 
 }
